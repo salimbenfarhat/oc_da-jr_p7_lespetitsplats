50f72b03957186ef1e3d372e5e9b94b172c0a70d
Salim Benfarhat <salim.draft@gmail.com>
2024-08-07 18:16:50 +0200

    07/0/2024 - Optimize the filterBySearch_v2 function

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
 // Version avec programmation fonctionnelle pour filtrer les recettes en fonction du terme de recherche
-function filterBySearch_v2(recipes, searchTerm) {
-    // Filtre le tableau des recettes
-    return recipes.filter(recipe => {
-      // Vérifie si le terme de recherche est inclus dans le titre ou la description de la recette
-      return recipe.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-             recipe.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-             // Vérifie si le terme de recherche est inclus dans l'un des ingrédients de la recette
-             recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(searchTerm.toLowerCase()));
-    });
-}  
+function filterBySearch_v2(_recipes, searchTerm) {
+  const lowerCaseSearchTerm = searchTerm.toLowerCase();
+
+  // Filtre le tableau des recettes
+  return _recipes.filter(recipe => {
+    // Convertit le nom et la description de la recette en minuscule une seule fois
+    const lowerCaseName = recipe.name.toLowerCase();
+    const lowerCaseDescription = recipe.description.toLowerCase();
+
+    // Vérifie si le terme de recherche est inclus dans le nom, la description ou les ingrédients de la recette
+    return lowerCaseName.includes(lowerCaseSearchTerm) ||
+           lowerCaseDescription.includes(lowerCaseSearchTerm) ||
+           recipe.ingredients.some(ingredient => {
+             const lowerCaseIngredient = ingredient.ingredient.toLowerCase();
+             return lowerCaseIngredient.includes(lowerCaseSearchTerm);
+           });
+  });
+}
